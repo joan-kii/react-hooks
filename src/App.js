b9afc0f7d3a0e1082b05bb24d5b2e35a5d1f413d
joan-kii <joan.sb81@gmail.com>
2020-11-21 04:56:40 +0100

    commit final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [color, setColor] = useState("black");
+
+  useEffect(() => {
+    const changeColorOnClick = () => {
+      if (color == "black"){
+        setColor("red");
+      } else {
+        setColor("black");
+      }
+    };
+
+    document.addEventListener("click", changeColorOnClick);
+
+    return () => {
+      document.removeEventListener("click", changeColorOnClick);
+    };
+  }, [color]);
+
   return (
-    <div className="App">
-      Hola Mundo!!!
+    <div>
+      <div
+        id="myDiv"
+        style={{
+          color: "white",
+          width: "100px",
+          height: "100px",
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          backgroundColor: color
+        }}>
+          Este div cambia de color. Clicka aquí!!!
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
+
